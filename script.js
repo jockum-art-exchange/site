002102b002d2aa4c17d25e348694a75cc103d770
jockum-art-exchange <nyaskolandesign@gmail.com>
2025-03-18 12:11:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ const recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
         console.log('reCAPTCHA completed successfully');
     }
 }, auth);
+
+// Initialize Firebase with your project config
+const firebaseConfig = {
+  apiKey: "AIzaSyAZJ7ZPNiKTj3pim2G7iJeOcIWe93BbdxU",
+  authDomain: "jockum-art-trade.firebaseapp.com",
+  projectId: "jockum-art-trade",
+  storageBucket: "jockum-art-trade.firebasestorage.app",
+  messagingSenderId: "660182457462",
+  appId: "1:660182457462:web:1a5bb2e3a93a55eac39357",
+  measurementId: "G-07BRPVLF47"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth(); // Initialize Firebase Auth
+const db = firebase.database(); // Initialize Firebase Database (if you use it)
+
