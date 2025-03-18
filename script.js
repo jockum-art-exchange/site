e23df7552033e3038d4b40bd3e1bc9dc9a3640eb
jockum-art-exchange <nyaskolandesign@gmail.com>
2025-03-18 11:57:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+import { RecaptchaVerifier } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
 
+// Initialize reCAPTCHA verifier
+const recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+    size: 'invisible', // or 'normal' if you want it to be visible
+    callback: (response) => {
+        console.log('reCAPTCHA completed successfully');
+    }
+}, auth);
